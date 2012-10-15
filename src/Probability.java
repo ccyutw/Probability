4497cd8e95c1bacbe0aa57d677bb9e39db927ca6
ccyutw <ccyutw@thoughtworks.com>
2012-10-15 13:07:12 +0530

    probability and or not

--- src/Probability.java
+++ src/Probability.java
@@ -1,6 +1,8 @@
+//Understand the chance of something happens
 public class Probability {
 
-    private double value; 
+    private double value;
+    private static final int MAX_PROBABILITY = 1;
 
     public Probability(double value) {
         this.value = value;
@@ -12,9 +14,12 @@ public class Probability {
 
     @Override
     public boolean equals(Object anotherProbability) {
-        if(anotherProbability == null) return false;
+        if(anotherProbability == null)
+            return false;
+
         if(this.getClass() != anotherProbability.getClass())
             return false;
+
         return this.value == ((Probability)anotherProbability).value;
 
     }
@@ -23,4 +28,24 @@ public class Probability {
     public int hashCode() {
         return Double.valueOf(value).hashCode();
     }
+
+    public Probability not() {
+        return new Probability(MAX_PROBABILITY - value);
+    }
+
+    public Probability and(Probability anotherProbability) {
+        return new Probability(value * anotherProbability.getProbability());
+    }
+
+    public Probability or(Probability anotherProbability) {
+        Probability firstProbability = new Probability(not().value);
+        Probability secondProbability = new Probability(anotherProbability.not().value);
+
+        Probability finalProbability = firstProbability.and(secondProbability);
+        return finalProbability.not();
+    }
 }
+
+
+
+
--- test/testProbability.java
+++ test/testProbability.java
@@ -1,20 +1,102 @@
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
+import static org.junit.Assert.*;
+//Ensures the  chance of something happens
 public class testProbability {
     @Test
-    public void shouldReturnTheValueOfProbability() throws Exception
+    public void shouldReturnTheValueOfProbability0dot4() throws Exception
     {
         Probability probability = new Probability(0.4);
         assertThat(probability.getProbability(), is(0.4));
+    }
+    @Test
+    public void shouldReturnTheValueOfProbability0dot3() throws Exception
+    {
+        Probability probability = new Probability(0.3);
+        assertThat(probability.getProbability(), is(0.3));
+    }
+    @Test
+    public void shouldReturnFalseWhenTryCompareWithANullObject() throws Exception
+    {
+        Probability probability = new Probability(0.4);
+        assertFalse(probability.equals(null));
+    }
+    @Test
+    public void shouldReturnFalseWhenTryCompareWithADifferentKindOfObject() throws Exception
+    {
+        Probability probability = new Probability(0.4);
+        assertFalse(probability.equals(0.4));
     }
     @Test
     public void shouldReturnTrueWhenTwoValuesAreEqual() throws Exception
     {
         Probability probability = new Probability(0.4);
         Probability anotherProbability = new Probability(0.4);
-        assertThat(probability,is(anotherProbability));
+        assertTrue(probability.equals(anotherProbability));
     }
+    @Test
+    public void shouldReturnFalseWhenTwoValuesAreDifferent() throws Exception
+    {
+        Probability probability = new Probability(0.4);
+        Probability anotherProbability = new Probability(0.5);
+        assertFalse(probability.equals(anotherProbability));
+
+    }
+    @Test
+    public void shouldReturnChanceOfOccurrenceNotHappenWhenChanceIs0dot1(){
+        Probability probability = new Probability(0.1);
+        Probability anotherProbability = probability.not();
+        assertTrue(anotherProbability.equals(new Probability(0.9)));
+    }
+    @Test
+    public void shouldReturnChanceOfOccurrenceNotHappenWhenChanceIs0dot2(){
+        Probability probability = new Probability(0.2);
+        Probability anotherProbability = probability.not();
+        assertTrue(anotherProbability.equals(new Probability(0.8)));
+    }
+    @Test
+    public void shouldReturnChanceOf0dot2And0dot5HappenAtSameTime(){
+        Probability probability = new Probability(0.2);
+        Probability anotherProbability = new Probability(0.5);
+
+        Probability expectedProbability = new Probability(0.1);
+        Probability bothProbability = probability.and(anotherProbability);
+
+        assertTrue(bothProbability.equals(expectedProbability));
+    }
+    @Test
+    public void shouldReturnChanceOf0dot4And0dot5HappenAtSameTime(){
+        Probability probability = new Probability(0.4);
+        Probability anotherProbability = new Probability(0.5);
+
+        Probability expectedProbability = new Probability(0.2);
+        Probability bothProbability = probability.and(anotherProbability);
+
+        assertTrue(bothProbability.equals(expectedProbability));
+    }
+
+    @Test
+    public void shouldReturnChanceOf0dot4or0dot5HappenAtSameTime(){
+        Probability probability = new Probability(0.4);
+        Probability anotherProbability = new Probability(0.5);
+
+        Probability expectedProbability = new Probability(0.7);
+        Probability bothProbability = probability.or(anotherProbability);
+
+        assertTrue(bothProbability.equals(expectedProbability));
+    }
+    @Test
+    public void shouldReturnChanceOf0dot2or0dot5HappenAtSameTime(){
+        Probability probability = new Probability(0.2);
+        Probability anotherProbability = new Probability(0.5);
+
+        Probability expectedProbability = new Probability(0.6);
+        Probability bothProbability = probability.or(anotherProbability);
+
+        assertTrue(bothProbability.equals(expectedProbability));
+    }
+
+
+
 }
